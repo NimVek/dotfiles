eac0e9269c8a8b7cc6e31bcbed438b55586e883f
NimVek <NimVek@users.noreply.github.com>
2021-11-10 13:23:22 +0100

    Check if fish is already the current shell

--- run_after_99_chsh.sh
+++ run_after_99_chsh.sh
@@ -6,5 +6,9 @@ set -o nounset
 FISH_BIN=$(command -v fish)
 
 if [[ -n ${FISH_BIN} ]]; then
-    chsh --shell "${FISH_BIN}"
+    CURRENT_SHELL=$(getent passwd "${USER}" | cut -d : -f 7)
+    if [[ ${FISH_BIN} -ne ${CURRENT_SHELL} ]]; then
+        echo "Changeing Login-Shell from ${CURRENT_SHELL} to ${FISH_BIN}"
+        chsh --shell "${FISH_BIN}"
+    fi
 fi
