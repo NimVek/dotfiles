31475f93de3af946f9128e4b9a05c83470abe3b4
NimVek <NimVek@users.noreply.github.com>
2021-11-11 22:18:39 +0100

    add some omf packages

--- run_after_25_install_omf.sh
+++ run_after_25_install_omf.sh
@@ -3,4 +3,11 @@
 set -o errexit
 set -o nounset
 
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish /dev/stdin --verbose --noninteractive --yes
+PACKAGES="powerline asdf"
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish /dev/stdin --noninteractive --yes
+
+fish -c "omf update"
+for i in ${PACKAGES}; do
+    fish -c "omf install ${i}"
+done
