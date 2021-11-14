84b2b5ee91b663f5cbd14a429301cb90645e7785
NimVek <NimVek@users.noreply.github.com>
2021-11-14 23:33:58 +0100

    add cargo-update

--- run_before_30_install_rustup.sh
+++ run_before_30_install_rustup.sh
@@ -3,10 +3,11 @@
 set -o errexit
 set -o nounset
 
-PLUGINS="topgrade"
+PLUGINS="cargo-update topgrade"
 
 if ! command -v rustup >/dev/null 2>&1; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --profile default
+    export PATH="${HOME}/.cargo/bin:${PATH}"
 fi
 
 for i in ${PLUGINS}; do
