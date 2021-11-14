34b11f8cf208f6e0af64c9fe1eb38d1672000a24
NimVek <NimVek@users.noreply.github.com>
2021-11-14 22:22:01 +0100

    add args to rustup install

--- run_before_30_install_rustup.sh
+++ run_before_30_install_rustup.sh
@@ -6,7 +6,7 @@ set -o nounset
 PLUGINS="topgrade"
 
 if ! command -v rustup >/dev/null 2>&1; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y --no-modify-path --profile default
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --profile default
 fi
 
 for i in ${PLUGINS}; do
